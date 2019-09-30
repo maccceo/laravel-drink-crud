3d13baeb0c44b9526896628bb9f1f7471636441d
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-09-30 22:23:47 +0200

    Aggiunto min,max,avg + funzione stampa corretta data

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -7,8 +7,29 @@ use App\Beverage;
 
 class DBController extends Controller
 {
+
     public function showDrink() {
     	$results = Beverage::all();
-    	return view('showDrink', compact('results'));
+    	// passa da formato YYYY-MM-DD a MM/YY
+    	$results = $this -> correctDateFormat($results);
+
+    	$max = Beverage::max('price');
+    	$min = Beverage::min('price');
+    	$avg = round(Beverage::avg('price'), 2);
+
+    	return view('showDrink', compact('results', 'max', 'min', 'avg'));
     }
+
+    private function correctDateFormat($results) {
+    	foreach ($results as $element) {
+    		$oldDate = $element -> expiring_date;
+    		// $oldDate diventa array con tre elementi: YYYY, MM, DD
+    		$oldDate = explode("-", $oldDate);
+    		// creo formato MM/YY
+    		$newDate = $oldDate[1] . "/" . substr($oldDate[0], -2);
+    		$element -> expiring_date = $newDate;
+    	}
+    	return $results;
+    }
+
 }
--- resources/views/showDrink.blade.php
+++ resources/views/showDrink.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 
-@foreach ($results as $element)
-	
-	<div class="box">
-		<p>{{ $element -> name }}</p>
-		<p>{{ $element -> brand }}</p>
-		<p>Prezzo: {{ $element -> price }}</p>
-		<p>Data di scadenza: {{ $element -> expiring_date }}</p>
-	</div>
-
-@endforeach
-
+	@foreach ($results as $element)
+		<div class="box">
+			<p>{{ $element -> name }}</p>
+			<p>{{ $element -> brand }}</p>
+			<p>Prezzo: {{ $element -> price }}</p>
+			<p>Data di scadenza: {{ $element -> expiring_date }}</p>
+		</div>
+	@endforeach
+	<hr>
+		<p>Prezzo massimo: <strong>{{ $max }}€</strong></p>
+		<p>Prezzo minimo: <strong>{{ $min }}€</strong></p>
+		<p>Prezzo medio: <strong>{{ $avg }}€</strong></p>
+		
 @endsection
